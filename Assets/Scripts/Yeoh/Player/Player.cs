c5895c464972f73a744442e33981b1bffdd67a34
JackS <90941636+JackSoundwave@users.noreply.github.com>
2024-02-04 11:31:49 +0800

    Revert "Yeoh"

--- Assets/Scripts/Yeoh/Player/Player.cs
+++ Assets/Scripts/Yeoh/Player/Player.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [HideInInspector] public PlayerStateMachine stateMachine;
-    [HideInInspector] public PlayerMovement move;
-    [HideInInspector] public ClosestObjectFinder finder;
-
-    public List<PlayerWeapon> hitboxes;
-
-    public bool canLook=true, canMove=true, canAttack=true;
-
-    void Awake()
-    {
-        stateMachine=GetComponent<PlayerStateMachine>();
-        move=GetComponent<PlayerMovement>();
-        finder=GetComponent<ClosestObjectFinder>();
-    }
+    public List<GameObject> targetsList = new List<GameObject>();
 }
--- Assets/Scripts/Yeoh/Player/PlayerMovement.cs
+++ Assets/Scripts/Yeoh/Player/PlayerMovement.cs
@@ -4,47 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Player player;
-    Rigidbody rb;
-
-    public FixedJoystick joystick;
+    [HideInInspector] public Rigidbody rb;
     [HideInInspector] public Vector3 dir;
+    public FixedJoystick joystick;
 
-    public float moveSpeed=10, acceleration=10, deceleration=10, velocity;
+    public float moveSpeed=10, acceleration=10, deceleration=10, velPower=1;
+    public bool canMove=true;
 
     void Awake()
     {
-        player=GetComponent<Player>();
         rb=GetComponent<Rigidbody>();
     }
 
-    // void Update()
-    // {
-    //     CheckInput();
-    // }
-
-    public void CheckInput()
+    void Update()
     {
-        if(joystick.Horizontal==0 && joystick.Vertical==0) // use keyboard wasd if joystick not touched
+        checkInputs();
+    }
+
+    void checkInputs()
+    {
+        if(joystick.Horizontal==0 && joystick.Vertical==0)
         {
-            dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+            dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         }
-        else // use joystick
+        else
         {
             dir = new Vector3(Mathf.Clamp(joystick.Horizontal, -1, 1), 0, Mathf.Clamp(joystick.Vertical, -1, 1));
         }
     }
 
-    public void NoInput()
-    {
-        dir = Vector3.zero;
-    }
-
     void FixedUpdate()
     {
-        velocity = rb.velocity.magnitude;
-
-        if(player.canMove)
+        if(canMove)
         {
             Vector3 camForward = Camera.main.transform.forward;
             Vector3 camRight = Camera.main.transform.right;
@@ -52,40 +43,21 @@ public class PlayerMovement : MonoBehaviour
             camForward.y=0;
             camRight.y=0;
 
-            Move(dir.z, camForward.normalized);
-            Move(dir.x, camRight.normalized);
+            move(dir.z, camForward.normalized);
+            move(dir.x, camRight.normalized);
         }
     }
 
-    void Move(float magnitude, Vector3 direction)
+    void move(float inputAxis, Vector3 moveAxis)
     {
-        float targetSpeed = magnitude * moveSpeed;
-
-        float accelRate = Mathf.Abs(targetSpeed)>0 ? acceleration:deceleration; // use decelerate value if no input, and vice versa
+        float targetSpeed = inputAxis*moveSpeed;
     
-        float speedDif = targetSpeed - Vector3.Dot(direction, rb.velocity); // difference between current and target speed
+        float speedDif = targetSpeed - Vector3.Dot(moveAxis, rb.velocity);
 
-        float movement = Mathf.Abs(speedDif) * accelRate * Mathf.Sign(speedDif); // slow down or speed up depending on speed difference
+        float accelRate = Mathf.Abs(targetSpeed)>0 ? acceleration:deceleration;
 
-        rb.AddForce(direction * movement);
+        float movement = Mathf.Pow(Mathf.Abs(speedDif)*accelRate, velPower)*Mathf.Sign(speedDif);
+
+        rb.AddForce(moveAxis*movement);
     }
-
-    public void Push(float force, Vector3 direction) //, float stopTime)
-    {
-        // if(disablingMoveRt!=null) StopCoroutine(disablingMoveRt);
-        // disablingMoveRt = StartCoroutine(DisablingMove(stopTime));
-
-        rb.velocity = new Vector3(0, rb.velocity.y, 0);
-
-        rb.AddForce(direction*force, ForceMode.Impulse);
-    }
-
-    // Coroutine disablingMoveRt;
-
-    // IEnumerator DisablingMove(float time)
-    // {
-    //     player.canMove=false;
-    //     yield return new WaitForSeconds(time);
-    //     player.canMove=true;
-    // }
 }
