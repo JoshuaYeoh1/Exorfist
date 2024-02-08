d26760068979107f2b82c617a89f6db348258229
JoshuaYeoh1 <137040922+JoshuaYeoh1@users.noreply.github.com>
2024-02-08 14:30:27 +0800

    organizing

--- Assets/Scripts/Yeoh/Camera/CineTouch.cs
+++ Assets/Scripts/Yeoh/Camera/CineTouch.cs
@@ -4,22 +4,31 @@ using Cinemachine;
 public class Cinetouch : MonoBehaviour
 {
     CinemachineFreeLook cineFreeLook;
-    public TouchField touchField;
+    TouchField touchField;
 
     public float SenstivityX = .2f, SenstivityY = -.2f;
 
     void Awake()
     {
         cineFreeLook=GetComponent<CinemachineFreeLook>();
+
+        touchField = GameObject.FindGameObjectWithTag("TouchField").GetComponent<TouchField>();
     }
 
     void Update()
     {
-        if(touchField && cineFreeLook)
+        if(touchField)
         {
-            cineFreeLook.m_XAxis.Value += touchField.TouchDist.x * 200 * SenstivityX * Time.deltaTime;
+            if(cineFreeLook)
+            {
+                cineFreeLook.m_XAxis.Value += touchField.TouchDist.x * 200 * SenstivityX * Time.deltaTime;
             
-            cineFreeLook.m_YAxis.Value += touchField.TouchDist.y * SenstivityY * Time.deltaTime;
+                cineFreeLook.m_YAxis.Value += touchField.TouchDist.y * SenstivityY * Time.deltaTime;
+            }
+        }
+        else
+        {
+            Debug.Log("Bruh where's the touch field?");
         }
     }
 }
