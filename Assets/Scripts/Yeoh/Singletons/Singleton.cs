a5508b9073a7c692d0e4a4101c649c61ef61cea4
JoshuaYeoh1 <137040922+JoshuaYeoh1@users.noreply.github.com>
2024-03-21 22:36:26 +0800

    try 60 again

--- Assets/Scripts/Yeoh/Singletons/Singleton.cs
+++ Assets/Scripts/Yeoh/Singletons/Singleton.cs
@@ -15,12 +15,12 @@ public class Singleton : MonoBehaviour
         }
         else Destroy(gameObject);
         
-        //Invoke("UnlockFPS", .1f); // FREEZES MY S10 AFTER PLAYING A WHILE
+        Invoke("UnlockFPS", .1f); // FREEZES MY S10 AFTER PLAYING A WHILE
     }
 
     void UnlockFPS()
     {
-        Application.targetFrameRate = 45;
+        Application.targetFrameRate = 60;
         QualitySettings.vSyncCount = 0;
     }
 
